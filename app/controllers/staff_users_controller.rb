b1fd03715a3a2c1f145716484989c640aae9454b
Swati Jain <jain.swati71@gmail.com>
2024-12-07 18:28:47 +0100

    created customers, staffusers and dashboard controllers

---
+++ app/controllers/staff_users_controller.rb
@@ -0,0 +1,2 @@
+class StaffUsersController < ApplicationController
+end
---
+++ test/controllers/staff_users_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class StaffUsersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
