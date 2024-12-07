24787d39e8c9d4a96e5b08b7d60fdd834894cbe0
Swati Jain <jain.swati71@gmail.com>
2024-12-07 18:15:59 +0100

    added data in admin table

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,7 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Role.create(name: "Customer")
-Role.create(name: "SuperUser")
+# Role.create(name: "Customer")
+# Role.create(name: "SuperUser")
+
+Admin.create(email: "admin@gmail.com", username: 'Admin', admin_type: 0, password: '123456', password_confirmation: '123456', role_id:2)
