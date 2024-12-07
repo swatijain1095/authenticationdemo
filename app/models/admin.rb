44d56685cc6121f0749fb84962f719c7ad3a7bce
Swati Jain <jain.swati71@gmail.com>
2024-12-07 18:26:03 +0100

    configured associations b/w role and admin models

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,17 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         enum admin_type: [:super_admin, :customer, :staff_user]
+
+         belongs_to :role, optional: true
+
+         before_save :manage_role
+
+         def manage_role
+          if self.customer?
+            self.role_id = 1
+          end
+        end
+
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,4 @@
 class Role < ApplicationRecord
+  has_many :admins
+  validates :name, presence: true, uniqueness: true
 end
