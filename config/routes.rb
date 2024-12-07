c01c4fd4ac4cae971d579996a273c9ca402de230
Swati Jain <jain.swati71@gmail.com>
2024-12-07 18:34:01 +0100

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: { sessions: 'admins/sessions', registrations: 'admins/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,4 +12,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :roles
+  resources :staff_users
+  resources :customers, only: [:edit, :update]
+  resources :dashboard, only: [:index]
+  root "dashboard#index"
 end
