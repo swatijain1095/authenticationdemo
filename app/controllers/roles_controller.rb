52d4ee4652270428bbb9e71e72337d264ab3c9eb
Swati Jain <jain.swati71@gmail.com>
2024-12-08 16:03:18 +0100

    editing roles by superuser

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,5 +1,6 @@
 class RolesController < ApplicationController
   before_action :authenticate_admin!
+  before_action :find_role, only: [:edit, :update]
   def index
     @roles = Role.all
   end
@@ -19,7 +20,29 @@ class RolesController < ApplicationController
     end
   end
 
+  def edit
+
+  end
+
+  def update
+		if(@role.update(role_params))
+			flash[:notice] = 'Role updated successfully'
+			redirect_to roles_path
+		else
+			flash[:notice] = @role.errors.full_messages.join(",")
+			redirect_to role_path(@role)
+		end
+	end
+
+  def find_role
+    @role = Role.find_by id: params[:id]
+    unless @role
+      flash[:notice] = "Role not found!!!"
+      redirect_to roles_path
+    end
+
   def role_params
     params.require(:role).permit(:name)
   end
+end
 end
