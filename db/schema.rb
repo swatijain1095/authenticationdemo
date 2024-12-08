22cf2b0efb26528c22d60006c9136777b259bef0
Swati Jain <jain.swati71@gmail.com>
2024-12-08 18:05:52 +0100

    created new model product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_08_163942) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_08_170519) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -132,6 +132,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_08_163942) do
 
   create_table "permissions", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.string "sku"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
