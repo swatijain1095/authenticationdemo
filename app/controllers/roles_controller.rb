cafd79eaebedc5abcc29601a6b3c1f3295383afa
Swati Jain <jain.swati71@gmail.com>
2024-12-08 15:54:00 +0100

    creating new roles by superuser

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -3,4 +3,23 @@ class RolesController < ApplicationController
   def index
     @roles = Role.all
   end
+
+  def new
+    @role = Role.new
+  end
+
+  def create
+    @role = Role.new(role_params)
+    if @role.save
+      flash[:notice] = 'Role created successfully'
+      redirect_to roles_path
+    else
+      flash[:notice] = @role.errors.full_messages.join(",")
+      redirect_to new_role_path
+    end
+  end
+
+  def role_params
+    params.require(:role).permit(:name)
+  end
 end
