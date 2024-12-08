8005639bc2b79315b76dbd18aa94bef2839b522f
Swati Jain <jain.swati71@gmail.com>
2024-12-08 18:02:44 +0100

    configured pundit gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  include Pundit::Authorization
+
+
+  rescue_from Pundit::NotAuthorizedError, with: :admin_not_authorized
+
+		def admin_not_authorized
+			flash[:alert] = "You are not authorized Person to perform this action."
+			redirect_to root_path
+		end
+    
+    def pundit_user
+      current_admin
+    end
 end
